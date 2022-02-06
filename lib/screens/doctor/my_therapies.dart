4cc86e0ab4e92c0ea04a5cf17c9321bd5f81f325
Mohit <mohit.kumar@vcareall.com>
2022-02-06 16:37:27 +0000

    My therapies implemented

--- lib/screens/doctor/my_therapies.dart
+++ lib/screens/doctor/my_therapies.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:untitled3/models/my_therapies_model.dart';
+import 'package:untitled3/models/treatment_list.dart';
+import 'package:untitled3/network/api_blocs.dart';
+import 'package:untitled3/network/api_urls.dart';
 
 class MyTherapies extends StatefulWidget {
   const MyTherapies({Key? key}) : super(key: key);
@@ -11,9 +15,350 @@ class _MyTherapiesState extends State<MyTherapies> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: Text('My Therapies'),
+      backgroundColor: Colors.blue,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          top: false,
+          child: Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+                decoration: BoxDecoration(
+                  color: Color(0xfff7f7f7), //s.grey,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                  ),
+                ),
+                child: Container(
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.03),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                left: MediaQuery.of(context).size.width * 0.05,
+                                top: 20,
+                              ),
+                              child: Icon(
+                                Icons.arrow_back,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.all(8),
+                              margin: EdgeInsets.only(
+                                left: MediaQuery.of(context).size.width * 0.18,
+                                top: 5,
+                              ),
+                              // alignment: Alignment.center,
+                              child: Text(
+                                'My Therapies',
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 22),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.2,
+                ),
+                child: FutureBuilder(
+                  future: commonBloc.hitGetApi(
+
+                      ///  'https://run.mocky.io/v3/eb5f43d1-0e08-48c1-be04-292dae6b8138'),
+                      ApiUrl.treatment_list + '?th_id=' + "2"), //widget.cId),
+                  builder: (context, AsyncSnapshot snap) {
+                    if (snap.data == null) {
+                      return Container(
+                        // height: MediaQuery.of(context).size.height * 0.3,
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    } else {
+                      MyTherapiesModel treatmentList =
+                          MyTherapiesModel.fromJson(snap.data);
+                      return ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: treatmentList.data.length,
+                        itemBuilder: (context, position) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.transparent,
+                              ),
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            margin: EdgeInsets.only(
+                              top: MediaQuery.of(context).size.height * 0.01,
+                              bottom: MediaQuery.of(context).size.height * 0.01,
+                              left: MediaQuery.of(context).size.width * 0.05,
+                              right: MediaQuery.of(context).size.width * 0.05,
+                            ),
+                            padding: EdgeInsets.only(
+                              top: MediaQuery.of(context).size.height * 0.02,
+                              bottom: MediaQuery.of(context).size.height * 0.02,
+                              //left: MediaQuery.of(context).size.width * 0.05,
+                              right: MediaQuery.of(context).size.width * 0.05,
+                            ),
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(15),
+                              ),
+                              child: Row(
+                                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    //mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Center(
+                                        child: Container(
+                                            margin: EdgeInsets.only(
+                                              left: MediaQuery.of(context)
+                                                      .size
+                                                      .width *
+                                                  0.05,
+                                            ),
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.20,
+                                            height: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.20,
+                                            decoration: new BoxDecoration(
+                                                border: Border.all(
+                                                  color: Colors.white,
+                                                ),
+                                                shape: BoxShape.circle,
+                                                image: new DecorationImage(
+                                                    fit: BoxFit.fill,
+                                                    image: new NetworkImage(
+                                                        "https://th.bing.com/th/id/OIP.hw-Sk04AflX4Te0r8K4R9QAAAA?pid=ImgDet&rs=1")))),
+                                      ),
+                                      SizedBox(
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.01,
+                                      ),
+                                      Row(
+                                        children: [
+                                          Container(
+                                            margin: EdgeInsets.only(
+                                              bottom: MediaQuery.of(context)
+                                                      .size
+                                                      .height *
+                                                  0.01,
+                                              left: 5
+                                            ),
+                                            color: Colors.grey,
+                                            child: Text(
+                                              " Age: "+treatmentList.data[position].age +" "+treatmentList.data[position].gender[0]+" ",
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: MediaQuery.of(context).size.width *
+                                        0.07,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        treatmentList
+                                            .data[position].patientName,
+                                        style: TextStyle(
+                                            color: Colors.blue,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16),
+                                      ),
+                                      SizedBox(height: 6),
+                                      Text(
+                                        treatmentList.data[position].therapy,
+                                        style: TextStyle(
+                                          color: Colors.grey,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(height: 10),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.calendar_today_sharp,
+                                            size: 16,
+                                            color: Colors.blue,
+                                          ),
+                                          SizedBox(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.025,
+                                          ),
+                                          Text(
+                                            treatmentList.data[position]
+                                                    .appointmentDate +
+                                                " | " +
+                                                treatmentList.data[position]
+                                                    .appointmentTime,
+                                            style: TextStyle(
+                                              fontSize: 10,
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      SizedBox(height: 10),
+                                      Row(
+                                        children: [
+                                          InkWell(
+//                                      onTap:(){
+//                                        Navigator.push(
+//                                          context,
+//                                          MaterialPageRoute(
+//                                              builder: (context) => OngoingTreatmentList()),
+//                                        );
+//                                      },
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                color: Colors.green,
+                                                borderRadius:
+                                                    BorderRadius.circular(8.0),
+                                              ),
+                                              child: Padding(
+                                                padding: EdgeInsets.only(
+                                                  left: MediaQuery.of(context)
+                                                          .size
+                                                          .width *
+                                                      0.04,
+                                                  right: MediaQuery.of(context)
+                                                          .size
+                                                          .width *
+                                                      0.04,
+                                                  top: MediaQuery.of(context)
+                                                          .size
+                                                          .height *
+                                                      0.008,
+                                                  bottom: MediaQuery.of(context)
+                                                          .size
+                                                          .height *
+                                                      0.008,
+                                                ),
+                                                child: Center(
+                                                  child: Text(
+                                                    'Confirm',
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.025,
+                                          ),
+                                          InkWell(
+//                                      onTap:(){
+//                                        Navigator.push(
+//                                          context,
+//                                          MaterialPageRoute(
+//                                              builder: (context) => AppointmentDetails()),
+//                                        );
+//                                      },
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                border: Border.all(
+                                                  color: Colors.transparent,
+                                                ),
+                                                color: Colors.redAccent,
+                                                borderRadius:
+                                                    BorderRadius.circular(8.0),
+                                              ),
+                                              child: Padding(
+                                                padding: EdgeInsets.only(
+                                                  left: MediaQuery.of(context)
+                                                          .size
+                                                          .width *
+                                                      0.04,
+                                                  right: MediaQuery.of(context)
+                                                          .size
+                                                          .width *
+                                                      0.04,
+                                                  top: MediaQuery.of(context)
+                                                          .size
+                                                          .height *
+                                                      0.008,
+                                                  bottom: MediaQuery.of(context)
+                                                          .size
+                                                          .height *
+                                                      0.008,
+                                                ),
+                                                child: Center(
+                                                  child: Text(
+                                                    'Cancel',
+                                                    style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    } //do whatever you want
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
